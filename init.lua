b965291067fec8ba47548b70c37b19506f21bca3
John Runyard <jrunyard3ft@gmail.com>
2024-06-05 13:57:35 -0500

    Changed theme to gruvbox.

--- init.lua
+++ init.lua
@@ -992,10 +992,35 @@ require('lazy').setup({
     end,
   }, ]]
   {
-    'chriskempson/base16-vim',
+    'ellisonleao/gruvbox.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
-      vim.cmd.colorscheme 'base16-default-dark'
+      require('gruvbox').setup {
+        terminal_colors = true, -- add neovim terminal colors
+        undercurl = true,
+        underline = true,
+        bold = true,
+        italic = {
+          strings = true,
+          emphasis = true,
+          comments = true,
+          operators = false,
+          folds = true,
+        },
+        strikethrough = true,
+        invert_selection = false,
+        invert_signs = false,
+        invert_tabline = false,
+        invert_intend_guides = false,
+        inverse = true, -- invert background for search, diffs, statuslines and errors
+        contrast = 'soft', -- can be "hard", "soft" or empty string
+        palette_overrides = {},
+        overrides = {},
+        dim_inactive = false,
+        transparent_mode = false,
+      }
+      vim.o.background = 'dark'
+      vim.cmd.colorscheme 'gruvbox'
       vim.cmd.set 'termguicolors'
     end,
   },
