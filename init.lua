7a2260e4e465f452f210e060c99daae86550155f
John Runyard <jrunyard3ft@gmail.com>
2024-11-25 22:29:20 -0600

    Added preview url to md.

--- init.lua
+++ init.lua
@@ -302,6 +302,7 @@ require('lazy').setup({
       vim.g.mkdp_auto_start = 1
       vim.g.mkdp_auto_close = 1
       vim.g.mkdp_refresh_slow = 1
+      vim.g.mkdp_echo_preview_url = 1
     end,
     ft = { 'markdown' },
   },
