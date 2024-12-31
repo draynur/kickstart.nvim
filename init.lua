e29ac7e8128fde1e80d5f09e1b094cb2dba2138c
John Runyard <jrunyard3ft@gmail.com>
2024-12-31 00:59:37 -0600

    Updated background config.

--- init.lua
+++ init.lua
@@ -1041,24 +1041,6 @@ require('lazy').setup({
       }
     end,
   },
-
-  --[[ { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
-    end,
-  }, ]]
   {
     'rebelot/kanagawa.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
@@ -1082,14 +1064,8 @@ require('lazy').setup({
           return {}
         end,
         theme = 'wave', -- Load "wave" theme when 'background' option is not set
-        background = { -- map the value of 'background' option to a theme
-          dark = 'wave', -- try "dragon" !
-          light = 'lotus',
-        },
       }
-      vim.o.background = 'dark'
       vim.cmd.colorscheme 'kanagawa'
-      vim.cmd.set 'termguicolors'
     end,
   },
 
