0b8b86dd43bcb8466f2eb39ab65e6d485fce4079
John Runyard <jrunyard3ft@gmail.com>
2025-01-08 15:32:29 -0600

    Turned off auto-start for md preview.

--- init.lua
+++ init.lua
@@ -299,7 +299,7 @@ require('lazy').setup({
     build = 'cd app && yarn install',
     init = function()
       vim.g.mkdp_filetypes = { 'markdown' }
-      vim.g.mkdp_auto_start = 1
+      vim.g.mkdp_auto_start = 0
       vim.g.mkdp_auto_close = 1
       vim.g.mkdp_refresh_slow = 1
       vim.g.mkdp_echo_preview_url = 1
