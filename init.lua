286b9481daae6d3d27131868a1f3ba4c8bb837ef
John Runyard <jrunyard3ft@gmail.com>
2025-02-05 09:18:46 -0600

    Added stop after first for js formatters.

--- init.lua
+++ init.lua
@@ -963,7 +963,7 @@ require('lazy').setup({
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        javascript = { 'prettierd', 'prettier' },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
         vue = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
