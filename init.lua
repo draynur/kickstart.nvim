0b0d5f06b919233f9c0a17e40669524fc140b356
John Runyard <jrunyard3ft@gmail.com>
2025-02-13 14:03:12 -0600

    Swapped theme.

--- init.lua
+++ init.lua
@@ -1113,10 +1113,9 @@ require('lazy').setup({
         overrides = function(colors) -- add/modify highlights
           return {}
         end,
-        theme = 'wave', -- Load "wave" theme when 'background' option is not set
+        theme = 'dragon', -- Load "wave" theme when 'background' option is not set
         background = { -- map the value of 'background' option to a theme
-          dark = 'wave', -- try "dragon" !
-          light = 'lotus',
+          dark = 'dragon', -- try "dragon" !
         },
       }
       vim.o.background = 'dark'
