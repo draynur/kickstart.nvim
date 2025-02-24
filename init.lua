f9d0e5cd87132977ffe550f5d2b098a8f02f17a8
John Runyard <jrunyard3ft@gmail.com>
2025-02-24 10:04:46 -0600

    Added new theme.

--- init.lua
+++ init.lua
@@ -1092,6 +1092,55 @@ require('lazy').setup({
     end,
   }, ]]
   {
+    'navarasu/onedark.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    init = function()
+      require('onedark').setup {
+        -- Main options --
+        style = 'dark', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+        transparent = true, -- Show/hide background
+        term_colors = true, -- Change terminal color as per the selected theme style
+        ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
+        cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+
+        -- toggle theme style ---
+        toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+        toggle_style_list = { 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light' }, -- List of styles to toggle between
+
+        -- Change code style ---
+        -- Options are italic, bold, underline, none
+        -- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
+        code_style = {
+          comments = 'italic',
+          keywords = 'none',
+          functions = 'none',
+          strings = 'none',
+          variables = 'none',
+        },
+
+        -- Lualine options --
+        lualine = {
+          transparent = false, -- lualine center bar transparency
+          theme = 'onedark',
+        },
+
+        -- Custom Highlights --
+        colors = {}, -- Override default colors
+        highlights = {}, -- Override highlight groups
+
+        -- Plugins Config --
+        diagnostics = {
+          darker = true, -- darker colors for diagnostic
+          undercurl = true, -- use undercurl instead of underline for diagnostics
+          background = true, -- use background color for virtual text
+        },
+      }
+      -- vim.o.background = 'dark'
+      vim.cmd.colorscheme 'onedark'
+      vim.cmd.set 'termguicolors'
+    end,
+  },
+  {
     'rebelot/kanagawa.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
@@ -1118,9 +1167,9 @@ require('lazy').setup({
           dark = 'dragon', -- try "dragon" !
         },
       }
-      vim.o.background = 'dark'
-      vim.cmd.colorscheme 'kanagawa'
-      vim.cmd.set 'termguicolors'
+      -- vim.o.background = 'dark'
+      -- vim.cmd.colorscheme 'kanagawa'
+      -- vim.cmd.set 'termguicolors'
     end,
   },
 
