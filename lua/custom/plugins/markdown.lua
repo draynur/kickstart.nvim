37aabffe89b9be1507fd2b98e6bbe8ae9cc493dd
John Runyard <jrunyard3ft@gmail.com>
2026-07-30 09:38:46 -0500

    Switched to main branch for treesitter

--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -3,7 +3,7 @@ return {
     'MeanderingProgrammer/markdown.nvim',
     name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
     dependencies = {
-      'nvim-treesitter/nvim-treesitter', -- Mandatory
+      { 'nvim-treesitter/nvim-treesitter', branch = 'main' }, -- Mandatory
       'nvim-tree/nvim-web-devicons', -- Optional but recommended
     },
     config = function()
